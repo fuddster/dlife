f49cf9d10bf23eaaffae4d30580467bbe63b1e6b
fuddster <fuddster@users.noreply.github.com>
2017-10-25 17:35:12 -0500

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,4 +1,15 @@
 var assert = require('assert');
+var bs = require('../lib/bs');
+
+describe('bs.js', function() {
+  describe('BloodSugar', function() {
+    var bloodsugar = bs.BloodSugar();
+    it('should have no insulin on board', function() {
+      assert.equal(0, bloodsugar.insulin);
+    });
+  });
+});
+
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
